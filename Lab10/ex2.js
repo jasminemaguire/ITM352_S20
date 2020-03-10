0e59289bddff486a5653dba8012dca49d439e0cb
Jasmine <jmaguire@hawaii.edu>
2020-03-09 22:06:15 -1000

    update

---
+++ Lab10/ex2.js
@@ -0,0 +1,2 @@
+product_quantities = [0, 1, 1, 3, 12];
+product_quantities[0];
---
+++ Lab9/ex1.js
@@ -0,0 +1,7 @@
+step1 = 98;
+step2 = parseInt(step1/4);
+step3 = step1 + step2;
+step4 = 4;
+step6 = step3 + step4;
+step7 = step6 + 7;
+finalTotal = step7%7;
